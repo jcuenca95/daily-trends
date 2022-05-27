5ff27ff914258842ea69256ae9b67ed7e05970fe
jcuenca95 <42175335+jcuenca95@users.noreply.github.com>
2022-05-27 22:36:22 +0200

    feat(feed-readers): elmundo reader working

--- feed-readers/readers/el-mundo.reader.ts
+++ feed-readers/readers/el-mundo.reader.ts
@@ -1,8 +1,27 @@
+import axios from "axios";
+import { load } from "cheerio";
 import { NewsDto } from "../../lib/news.dto";
 import { FeedReader } from "../core/feed-reader";
 
 export class ElMundoReader extends FeedReader {
     public async obtainData(): Promise<NewsDto[]> {
-        return []
+        const { data } = await axios.get<string>(this.url);
+        const $ = load(data);
+        const h2List = $('article');
+        h2List.length = 5;
+        const top5Links = h2List.map((i, el) => $(el).find('a').attr('href'));
+        return Promise.all(top5Links.map((i, link) => this.readArticle(link)));
+    }
+
+    private async readArticle(link: string): Promise<NewsDto> {
+        const { data } = await axios.get<string>(link);
+        const $ = load(data);
+        return {
+            title: $('h1').text(),
+            body: $('.ue-c-article__body > p').text(),
+            image: $('article img').attr('src') || '',
+            source: link,
+            publisher: $('.ue-c-article__byline-name > a').text()
+        }
     }
 }
