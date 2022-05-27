ee5f61ee0e4f629a616079e9f5f5a0a014de7dd1
jcuenca95 <42175335+jcuenca95@users.noreply.github.com>
2022-05-27 21:11:28 +0200

    fix(feed-readers): deleted console log

--- feed-readers/readers/el-pais.reader.ts
+++ feed-readers/readers/el-pais.reader.ts
@@ -12,7 +12,6 @@ export class ElPaisReader extends FeedReader {
         const h2List = $('h2');
         h2List.length = 5;
         const top5Links = h2List.map((i, el) => `${this.url}${$(el).find('a').attr('href')}`);
-        console.log(top5Links);
         return Promise.all(top5Links.map((i, link) => this.readArticle(link)));
     }
 
