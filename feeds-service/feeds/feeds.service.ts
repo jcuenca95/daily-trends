53b4f3b61ff0f89c5a84453d5d8614bd1ac89c31
jcuenca95 <42175335+jcuenca95@users.noreply.github.com>
2022-05-29 22:12:46 +0200

    feat(feeds-service): handling query param day

--- feeds-service/feeds/feeds.service.ts
+++ feeds-service/feeds/feeds.service.ts
@@ -8,4 +8,19 @@ export class FeedsService extends CrudService {
     constructor(feedRepository: FeedsRepository) {
         super(feedRepository)
     }
+
+    public findAll(filter: Record<string, any>): Promise<any[]> {
+        if (filter.day) {
+            const startDay = new Date(filter.day);
+            startDay.setHours(0, 0, 0, 0);
+            const endDay = new Date(startDay);
+            endDay.setDate(endDay.getDate() + 1);
+            filter.createdAt = {
+                $gte: startDay,
+                $lte: endDay
+            }
+        }
+        return this.repository.findAll(filter);
+    }
+
 }
